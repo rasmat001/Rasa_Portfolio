1ab1a796de998671c98b685f3d8d6a6f82a81d45
Rasa Matuliauskaitė <118724160+rasmat001@users.noreply.github.com>
2023-05-29 17:11:04 +0300

    Update Session Time Analysis (Marketing).sql

--- queries/Session Time Analysis (Marketing).sql
+++ queries/Session Time Analysis (Marketing).sql
@@ -29,37 +29,6 @@ cte2 AS (
 
 --calculating cost for paid marketing campaigns
 cte3 AS (
-WITH cte1 AS (							
-    SELECT *,							
-        SUM(is_new_session) OVER (ORDER BY user_pseudo_id,event_timestamp) global_session_id	--giving a unique id for each session					
-    FROM (							
-        SELECT *,							
-            CASE WHEN TIMESTAMP_DIFF(event_timestamp, last_event, MINUTE) >30							
-            OR last_event IS NULL							
-            THEN 1 ELSE 0 END is_new_session --flagging events where the a new session begins							
-        FROM (							
-            SELECT PARSE_DATE('%Y%m%d',event_date) event_date,							
-                TIMESTAMP_MICROS(event_timestamp) event_timestamp,							
-                event_name,							
-                user_pseudo_id,
-                event_value_in_usd,							
-                LAG(TIMESTAMP_MICROS(event_timestamp)) OVER							
-                (PARTITION BY user_pseudo_id ORDER BY event_timestamp) last_event,							
-                campaign							
-            FROM `tc-da-1.turing_data_analytics.raw_events` )))	,						
-
---calculating metrics for each session							
-cte2 AS (
-    SELECT global_session_id,							
-        MIN(event_timestamp) session_started_at,							
-        TIMESTAMP_DIFF(MAX(event_timestamp),MIN(event_timestamp), MINUTE) session_time,							
-        SUM(CASE WHEN event_name='purchase' AND event_value_in_usd>0 THEN 1 ELSE 0 END) purchases_during_session,
-        SUM(event_value_in_usd) revenue							
-    FROM cte1							
-    GROUP BY 1),
-
---calculating cost for paid marketing campaigns
-cte3 AS (
     SELECT campaign, SUM(cost) cost
     FROM `turing_data_analytics.adsense_monthly`
     WHERE Month  BETWEEN 202011 AND 202101 OR campaign='Holiday_V2'
